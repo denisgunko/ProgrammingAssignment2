7008bdad47fc23d2eb7258ecd502861bb93ad1af
denisgunko <denis.gunko@gmail.com>
2017-01-20 17:07:24 +0300

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  set <- function(y){
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) invm <<- inverse
+  getinv <- function() invm
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+
+# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invm <- x$getinv()
+  if(!is.null(invm)) {
+    message("getting cached data")
+    return(invm)
+  }
+  data <- x$get()
+  invm <- solve(data, ...)
+  x$setinv(invm)
+  invm
 }
+
+x <- rbind(c(1, 2), c(3, 4))
+x
+chm <- makeCacheMatrix(x)
+chm$get()
+cacheSolve(chm)
+cacheSolve(chm)
+
+# Test run:
+# > x <- rbind(c(1, 2), c(3, 4))
+# > x
+#      [,1] [,2]
+# [1,]    1    2
+# [2,]    3    4
+# > chm <- makeCacheMatrix(x)
+# > chm$get()
+#      [,1] [,2]
+# [1,]    1    2
+# [2,]    3    4
+# No cache
+# > cacheSolve(chm)
+#      [,1] [,2]
+# [1,] -2.0  1.0
+# [2,]  1.5 -0.5
+# Cached data has been used
+# > cacheSolve(chm)
+# getting cached data
+#      [,1] [,2]
+# [1,] -2.0  1.0
+# [2,]  1.5 -0.5
+# > 
